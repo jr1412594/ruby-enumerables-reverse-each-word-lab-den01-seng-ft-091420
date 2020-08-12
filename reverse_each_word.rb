c38a07334087319b1eac2c853a522c9c3a37073f
Gerardo Medina Jr <jr.medina1412@gmail.com>
2020-08-12 18:22:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,7 @@
 def reverse_each_word(sentence)
 array = sentence.split(" ")
 new_array = []
-array.each do |element|
-  new_array << element.reverse
-end
-new_array.join(" ")
+array.collect {|element| element.reverse
 end
 
  
