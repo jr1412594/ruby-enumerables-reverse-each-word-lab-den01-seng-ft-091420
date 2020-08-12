f6815b6b5c2c8f8f3107de164e87df27993f80f0
Gerardo Medina Jr <jr.medina1412@gmail.com>
2020-08-12 18:16:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,12 @@
 require 'pry'
 def reverse_each_word(sentence)
 array = sentence.split(" ")
-binding.pry
-return array
-
+new_array = []
+array.each do |element|
+  new_array << element.reverse
+end
+new_array
 end
 
  
- binding.pry
+ 
